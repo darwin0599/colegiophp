1be1c77021e186b914b74c277e3bfbcc9dbaa6fd
jsoler88 <jhonsoler888@gmail.com>
2020-11-10 22:25:17 -0500

    add footer and header in index.php

---
+++ footer.php
@@ -0,0 +1,94 @@
+    <!--Footer -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                    <h3>Mi Pequeño Mundo</h3>
+                    <p>
+                        Crr 8 # 14a-15<br>
+                        Barbosa, Santander <br>
+                        Colombia <br><br>
+                        <strong>Telefono:</strong> +57 3136052704<br>
+                        <strong>Email:</strong> info@example.com<br>
+                    </p>
+                    </div>
+                    <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Menú Final</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="index.php">Inicio</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="informacion.php">Nosotros Somos!</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="horarios.php">Matriculas</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="informacion.php">Perfil Institucional</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="noticias.php">Noticias</a></li>
+                    
+                    </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>
+                    <br>
+                    </h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="contenido.php">Observador Virtual</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="docentes.php">Docentes y Administrativos </a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="cronograma.php">Horarios</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="contactenos.php">Contactanos</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Inicio Sesion</a></li>
+                    </ul>
+                    </div>
+                    <div class="col-lg-4 col-md-6 footer-newsletter">
+                    <h4>Te contactamos</h4>
+                    <p>Mi pequeño mundo ofrece llamarte, si deseas conocer mas informacion sobre nuestros servicios digita tu NUMERO TELEFONICO.</p>
+                    <form class="border-content-blue" method="post">
+                        <input type="number" name="number"><input type="submit" value="Enviar Contacto">
+                    </form>
+                    </div>
+                </div>
+                <div class="row">
+                <div id="social-footer" class="d-flex align-items-center py-3 col-12">
+                    <div class="row">
+                        <div class="copyright d-flex justify-content-center col-12 col-xl-5">
+                            <p>&copy; Todos los Derechos Reservados<strong><span> Mi Pequeño Mundo</span></strong>.</p>
+                        </div>
+                        <div class="col-12 col-xl-7">
+                            <div class="row justify-content-md-center">
+                                <div class="contact-info col-12 col-md-6 col-lg-5 ">
+                                    <div class="row">
+                                    <div class="col-12 col-12 d-flex justify-content-center justify-content-md-start">
+                                        <a href="#">                                    <i class="fas fa-map-marker-alt"></i>Crr 8 # 14a-15 Barbosa, Santander.</a>
+                                    </div>
+                                    <div class="col-12 d-flex justify-content-center justify-content-md-start">
+                                        <a href="mailto:contact@example.com" target="_blank"><i class="far fa-envelope-open"></i> contact@example.com</a>
+                                    </div>
+                                    <div class="col-12 col-12 d-flex justify-content-center justify-content-md-start">
+                                        <a href="https://api.whatsapp.com/send?phone=573136052704&text=Hola!%20Quiero%20generar%20mas%20ventas!" target="_blank"><i class="fab fa-whatsapp"></i> +57 3136052704</a>
+                                    </div>
+                                    </div>
+                                </div>
+                                <div class="social-footer-network d-flex justify-content-center align-items-center col-12 col-md-6 col-lg-6">
+                                    <a href="#" class="twitter mx-1" target="_blank"><i class="fab fa-twitter"></i></a>
+                                    <a href="#" class="facebook mx-1"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#" class="instagram mx-1"><i class="fab fa-instagram"></i></a>
+                                    <a href="#" class="google-plus mx-1"><i class="fab fa-google-plus-g"></i></a>
+                                    <a href="#" class="linkedin mx-1"><i class="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+    </footer><!-- End Footer -->
+  
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+          crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+          integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+          crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+          integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+          crossorigin="anonymous"></script>
+  </body>
+</html>
