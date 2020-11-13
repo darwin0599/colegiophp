40a6b15bbcf20a598cc80fea4ecdb24919c84f04
darwin0599 <darentimput1999@gmail.com>
2020-11-12 23:34:10 -0500

    eventos y noticias

--- eventos.php
+++ eventos.php
@@ -1,89 +1,65 @@
 <?php include ('header.php'); ?>
-    <section> <!--___sección  eventos____-->
-      <div class="container ">
-         <div class="border-content-blue pt-2 pb-2 mt-2 mb-3">
-          <h3 class="text-center">EVENTOS</h3>
-          <p class="italic text-center">Centro Educativo</p>
-        </div>
-        <div class="row mb-2">
-              <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-center border-content-blue">
-                <img width="450" height="315" src="images/almuerzo niños.jpg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
+    <section> <!------ Sección  Eventos ------->
+    <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="h2 text-center color-blue bg-color-blue-light text-white mt-5 p-2">Eventos</p>
+                </div>
+                <div class="col-12">
+                    <p class="text-center color-blue bg-color-blue-light text-white p-2">Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+                    consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.</p>
+                </div>
+            </div>
+          <div class="container mt-3">  
+            <div class="row">
+              <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-center">
+                <img width="450" height="315" src="images/almuerzo_niños.jpg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
                 <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
               </div>
-          <div class="col-xl-6 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 border-content">
-            <div class="icon-box">
+              <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light ">
               <div class="icon"><i class="bx bx-fingerprint"></i></div>
-              <h4> Estudios muestran los beneficios de comer acompañado
+              <h4 > Estudios muestran los beneficios de comer acompañado
               </h4>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam porro dolores recusandae molestias aspernatur praesentium nisi modi excepturi! Eos consequuntur sequi sapiente dolore numquam alias, ipsa laudantium? Doloremque, delectus!
-                    </p>
-              </div>
-            </div>
-       </div>
-      </div>
-    </section>
-    <section>
-        <div class="container">
-          <div class="row mb-2">
-            <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-center border-content">
-              <img width="450" height="315" src="https://www.youtube.com/embed/BD4qBctPHzU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
-              <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
-            </div>
-        <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 border-content-blue">
-          <div class="icon-box">
-            <div class="icon"><i class="bx bx-fingerprint"></i></div>
-            <h4> Estudios muestran los beneficios de comer acompañado
-            </h4>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam porro dolores recusandae molestias aspernatur praesentium nisi modi excepturi! Eos consequuntur sequi sapiente dolore numquam alias, ipsa laudantium? Doloremque, delectus!
-                  </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section>
-        <div class="container ">
-          <div class="row mb-2">
-                <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-center border-content-blue">
-                  <img width="450" height="315" src="images/almuerzo niños.jpg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
-                  <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
-                </div>
-            <div class="col-xl-6 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 border-content">
-              <div class="icon-box">
-                <div class="icon"><i class="bx bx-fingerprint"></i></div>
-                <h4> Estudios muestran los beneficios de comer acompañado
-                </h4>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam porro dolores recusandae molestias aspernatur praesentium nisi modi excepturi! Eos consequuntur sequi sapiente dolore numquam alias, ipsa laudantium? Doloremque, delectus!
-                      </p>
-                </div>
-              </div>
-         </div>
-        </div>
-      </section>
-      <section>
-          <div class="container">
-            <div class="row mb-2">
-              <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-center border-content">
-                <img width="450" height="315" src="images/almuerzo niños.jpg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
-                <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
-              </div>
-          <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 border-content-blue">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-fingerprint"></i></div>
-              <h4> Estudios muestran los beneficios de comer acompañado
-              </h4>
-                    <p>
+                    <p class="text-white">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam porro dolores recusandae molestias aspernatur praesentium nisi modi excepturi! Eos consequuntur sequi sapiente dolore numquam alias, ipsa laudantium? Doloremque, delectus!
                     </p>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section>
-          
+          <div class="container mt-3">  
+            <div class="row">
+              <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-center">
+                <img width="450" height="315" src="images/almuerzo_niños.jpg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
+                <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
+              </div>
+              <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light ">
+              <div class="icon"><i class="bx bx-fingerprint"></i></div>
+              <h4 > Estudios muestran los beneficios de comer acompañado
+              </h4>
+                    <p class="text-white">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam porro dolores recusandae molestias aspernatur praesentium nisi modi excepturi! Eos consequuntur sequi sapiente dolore numquam alias, ipsa laudantium? Doloremque, delectus!
+                    </p>
+              </div>
+            </div>
+          </div>
+          <div class="container mt-3">  
+            <div class="row">
+              <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-center">
+                <img width="450" height="315" src="images/almuerzo_niños.jpg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
+                <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
+              </div>
+              <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light ">
+              <div class="icon"><i class="bx bx-fingerprint"></i></div>
+              <h4 > Estudios muestran los beneficios de comer acompañado
+              </h4>
+                    <p class="text-white">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam porro dolores recusandae molestias aspernatur praesentium nisi modi excepturi! Eos consequuntur sequi sapiente dolore numquam alias, ipsa laudantium? Doloremque, delectus!
+                    </p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>  <!------ Final Sección Eventos------->
+    
     <!--Footer -->    
   <?php include ('footer.php'); ?>
