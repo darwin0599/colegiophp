c76eaafbe90bf24216872f45adc98dfa1d539f6b
jsoler88 <jhonsoler888@gmail.com>
2020-11-13 22:42:12 -0500

    upload docentes and honores

--- docentes.php
+++ docentes.php
@@ -1,85 +1,117 @@
 <?php include ('header.php'); ?>
- <!-- ======= Seccion docentes======= -->
-  <section id="docentes" class="docentes">
-    
-      <div class="container mt-3 mb-3 ">
-        <div class="col-12">
-           <p class="h2 text-center color-blue mb-3 bg-color-blue-light text-white p-2">DOCENTES</p>
-           <p class="italic text-center"></p>
-        </div>
-      </div>   
-      <div class="container mt-3 mb-3">
-     </div>
-     <div class="container mt-3 mb-3">  
-        <div class="row ">
-          <div class="col-6 col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center ">
-            <img width="150" height="280" src="images/profe johana.jpeg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
-            <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
-          </div>
-        <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light text-white">
-          <div class="icon-box">
-            <div class="icon"><i class="bx bx-fingerprint justify-content-center"></i></div>
-            <h3>Alexandra Ibarra Aguirre</h3>
-                <span>Licenciada en Pedagogia Infantil</span>
-                <p> Docente </p>
-                <p>Centro Educativo Mi Pequeño Mundo </p>
-          </div>
-        </div>
-            <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center ">
-              <img width="150" height="280" src="images/trectora.jpeg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
-              <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
-            </div>
-            <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light text-white">
-              <div class="icon-box">
-              <div class="icon"><i class="bx bx-fingerprint"></i></div>
-              <h4>Cindy Nayive Diaz Quevedo </h4>
-                <span>Licenciada en Lengua <br> 
-                  Castellana y Comunicación</span>
-                <p>Docente y Rectora</p>
-                <p>Centro Educativo Mi Pequeño Mundo </p>
-            </div>
-          </div>
-      </div>
-    </div>
-    </section>
-  <section>
-  <div class="container mt-3 mb-3">
+<!-- ======= Seccion docentes======= -->
+<div class="container mt-3 mb-3 ">
+    <p class="h2 text-center color-blue mb-3 bg-color-blue-light text-white p-2">DOCENTES</p>
+    <p class="italic text-center"></p>
+</div>
+<div class="container mt-3 mb-3">
     <div class="row">
-        <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center ">
-          <img width="150" height="280" src="images/profe johana.jpeg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
-          <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
+        <div class="col-12 col-md-6 col-lg-3 mb-5 text-center">
+            <img width="200px" height="200px" src="images/trectora.jpeg" class="rounded-circle"></img>
+            <h5 class="color-blue mt-2 text-center">Cindy Nayive Diaz Quevedo</h5>
         </div>
-       <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light text-white">
-        <div class="icon-box">
-          <div class="icon"><i class="bx bx-fingerprint justify-content-center"></i></div>
-          <h4>Alexandra Ibarra Aguirre</h4>
-              <span>Licenciada en Pedagogia Infantil</span>
-              <br><p>Docente </p>
-              <p>Centro Educativo Mi Pequeño Mundo </p>
+        <div class="col-12 col-md-6 col-lg-3 mb-5">
+            <div class="row">
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-graduation-cap fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Licenciada en Lengua Castellana y Comunicación</p>
+                </div>
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-chalkboard-teacher fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Docente y Rectora</p>
+                </div>
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-school fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Centro Educativo Mi Pequeño Mundo</p>
+                </div>
+            </div>
         </div>
-      </div>
-          <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center">
-            <img width="150" height="280" src="images/trectora.jpeg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
-            <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
-          </div>
-          <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light text-white">
-            <div class="icon-box">
-            <div class="icon"><i class="bx bx-fingerprint"></i></div>
-            <h4>Cindy Nayive Diaz Quevedo </h4>
-              <span>Licenciada en Lengua <br> 
-                Castellana y Comunicación</span>
-              <p>Docente y Rectora</p>
-              <p>Centro Educativo Mi Pequeño Mundo </p>
-          </div>
+        <div class="col-12 col-md-6 col-lg-3 mb-5 text-center">
+            <img width="200px" height="200px" src="images/profe johana.jpeg" class="rounded-circle"></img>
+            <h5 class="color-blue mt-2 text-center">Alexandra Ibarra Aguirre</h5>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3 mb-5">
+            <div class="row">
+                <div class="col-3 d-flex align-items-center mb-4">
+                    <i class="fas fa-graduation-cap fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Licenciada en Pedagogia Infantil</p>
+                </div>
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-chalkboard-teacher fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Docente</p>
+                </div>
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-school fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Centro Educativo Mi Pequeño Mundo</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3 mb-5 text-center">
+            <img width="200px" height="200px" src="images/trectora.jpeg" class="rounded-circle"></img>
+            <h5 class="color-blue mt-2 text-center">Cindy Nayive Diaz Quevedo</h5>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3 mb-5">
+            <div class="row">
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-graduation-cap fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Licenciada en Lengua Castellana y Comunicación</p>
+                </div>
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-chalkboard-teacher fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Docente y Rectora</p>
+                </div>
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-school fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Centro Educativo Mi Pequeño Mundo</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3 mb-5 text-center">
+            <img width="200px" height="200px" src="images/profe johana.jpeg" class="rounded-circle"></img>
+            <h5 class="color-blue mt-2 text-center">Alexandra Ibarra Aguirre</h5>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3 mb-5">
+            <div class="row">
+                <div class="col-3 d-flex align-items-center mb-4">
+                    <i class="fas fa-graduation-cap fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Licenciada en Pedagogia Infantil</p>
+                </div>
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-chalkboard-teacher fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Docente</p>
+                </div>
+                <div class="col-3 d-flex align-items-center  mb-4">
+                    <i class="fas fa-school fa-2x color-blue"></i>
+                </div>
+                <div class="col-9 mb-4">
+                    <p class="m-0">Centro Educativo Mi Pequeño Mundo</p>
+                </div>
+            </div>
         </div>
     </div>
-  </div>
-  </section>
-    
-    <section>
+</div>
 
-    <!--Footer -->
+<!--Footer -->
 <?php include ('footer.php'); ?>
-
-
-
--- honores.php
+++ honores.php
@@ -1,75 +1,120 @@
 <?php include ('header.php'); ?>
-      <!-- ======= Seccion Cuadro Honor======= -->
-      <section id="honores" class="honores pt-3 pb-5">
-          <div class="container ">
-            <div class="section-title">
-              <h2 class="text-center mt-3 mb-3">Estudiantes Destacados</h2>
-              <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+<!-- ======= Seccion Cuadro Honor======= -->
+<div class="container mt-5">
+    <p class="h2 text-center color-blue mb-3 bg-color-blue-light text-white p-2">Estudiantes Destacados</p>
+    <div class="section-title">
+        <p class="h5">Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
+            in iste officiis commodi quidem hic quas.</p>
+    </div>
+    <div class="row justify-content-between my-3">
+        <div class="col-12">
+          <p class="h1 color-blue font-weight-blod">Honores JARDIN</p>
+          <hr>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div id="card" class="card" style="width: 100%">
+                <img src="images/1_niña_cuadro.jpg" class="card-img-top" alt="Imagen Estudiante">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-blod color-blue">Julian Amador</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
             </div>
-            <div class="row justify-content-between mt-3 mb-3">
-              <div class="col-10 col-md-5 col-lg-4">
-                <div id="card" class="card" style="width: 18rem;">
-                    <img src="images/circle-cropped.png" class="card-img-top" alt="Imagen Estudiante">
-                    <img src="images/3niño cuadro honor.jpg" class="card-img-top" alt="Imagen Estudiante">
-                    
-                    <div class="card-body">
-                        <h5 class="card-title">Julian Amador</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>  
-                  </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div id="card" class="card" style="width: 100%">
+                <img src="images/2_niña_cuadro.jpg" class="card-img-top" alt="Imagen Estudiante">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-blod color-blue">Julian Amador</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
                 </div>
-              <div class="col-10 col-md-5 col-lg-4">
-                <div id="card" class="card" style="width: 18rem;">
-                  <img src="images/niña cuadro honor.jpg" class="card-img-top" alt="Imagen Estudiante">
-                  <div class="card-body">
-                    <h5 class="card-title">Juliana Amador</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-10 col-md-5 col-lg-4">
-                <div id="card" class="card" style="width: 18rem;">
-                  <img src="images/3niño cuadro honor.jpg" class="card-img-top" alt="Imagen Estudiante">
-                  <div class="card-body">
-                    <h5 class="card-title">Julian Amador</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
-                </div>
-              </div>
             </div>
-            <div class="row justify-content-between  mt-3 mb-3">
-              <div class="col-10 col-md-5 col-lg-4">
-                <div id="card" class="card" style="width: 18rem;">
-                  <img src="images/2niña cuadro honor.jpg" class="card-img-top" alt="Imagen Estudiante">
-                  <div class="card-body">
-                    <h5 class="card-title">Camila Andrea</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div id="card" class="card" style="width: 100%">
+                <img src="images/3_niña_cuadro.jpg" class="card-img-top" alt="Imagen Estudiante">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-blod color-blue">Julian Amador</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
                 </div>
-              </div>
-              <div class="col-10 col-md-5 col-lg-4">
-                <div id="card" class="card" style="width: 18rem;">
-                  <img src="images/3niño cuadro honor.jpg" class="card-img-top" alt="Imagen Estudiante">
-                  <div class="card-body">
-                    <h5 class="card-title">Julian Amador</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-10 col-md-5 col-lg-4">
-                <div id="card" class="card" style="width: 18rem;">
-                  <img src="images/2niña cuadro honor.jpg" class="card-img-top" alt="Imagen Estudiante">
-                  <div class="card-body">
-                    <h5 class="card-title">Julian Amador</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
-        </section><!--====Final seccion Estudiantes=====  -->
-        <!-- ======= Menú Final======= -->
- 
-      
-   <!--Footer -->
-     <?php include ('footer.php'); ?>    
+        </div>
+    </div>
+    <div class="row justify-content-between my-3">
+        <div class="col-12">
+          <p class="h1 color-blue font-weight-blod">Honores PREJARDIN</p>
+          <hr>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div id="card" class="card" style="width: 100%">
+                <img src="images/1_niña_cuadro.jpg" class="card-img-top" alt="Imagen Estudiante">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-blod color-blue">Julian Amador</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div id="card" class="card" style="width: 100%">
+                <img src="images/2_niña_cuadro.jpg" class="card-img-top" alt="Imagen Estudiante">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-blod color-blue">Julian Amador</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div id="card" class="card" style="width: 100%">
+                <img src="images/3_niña_cuadro.jpg" class="card-img-top" alt="Imagen Estudiante">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-blod color-blue">Julian Amador</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-between my-3">
+        <div class="col-12">
+          <p class="h1 color-blue font-weight-blod">Honores PARVULOS</p>
+          <hr>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div id="card" class="card" style="width: 100%">
+                <img src="images/1_niña_cuadro.jpg" class="card-img-top" alt="Imagen Estudiante">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-blod color-blue">Julian Amador</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div id="card" class="card" style="width: 100%">
+                <img src="images/2_niña_cuadro.jpg" class="card-img-top" alt="Imagen Estudiante">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-blod color-blue">Julian Amador</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div id="card" class="card" style="width: 100%">
+                <img src="images/3_niña_cuadro.jpg" class="card-img-top" alt="Imagen Estudiante">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-blod color-blue">Julian Amador</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Footer -->
+<?php include ('footer.php'); ?>
