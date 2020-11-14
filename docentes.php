1e02b3f220d7e8f0321d6911258aaf8cc89e7888
darwin0599 <darentimput1999@gmail.com>
2020-11-13 20:11:14 -0500

    docentes horario avance

--- docentes.php
+++ docentes.php
@@ -2,54 +2,34 @@
  <!-- ======= Seccion docentes======= -->
   <section id="docentes" class="docentes">
     
-      <div class="section-title text-center mt-2">
-        <div class="container border-content mb-2 ">
-        <h2>Administrativos y Docentes</h2>
+      <div class="container mt-3 mb-3 ">
+        <div class="col-12">
+           <p class="h2 text-center color-blue mb-3 bg-color-blue-light text-white p-2">DOCENTES</p>
+           <p class="italic text-center"></p>
         </div>
-        <div class="container border-content-blue mt-3 mb-3 py-2 ">
-        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
-      </div>  
+      </div>   
       <div class="container mt-3 mb-3">
-      <div class="row ">
-            <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-center border-content-blue">
-              <img width="450" height="315" src="images/docentesludica.jpg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
-              <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
-            </div>
-        <div class="col-xl-6 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 border-content">
-          <div class="icon-box">
-            <div class="icon"><i class="bx bx-fingerprint"></i></div>
-            <h4> Estudios muestran los beneficios de comer acompañado
-            </h4>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam porro dolores recusandae molestias aspernatur praesentium nisi modi excepturi! Eos consequuntur sequi sapiente dolore numquam alias, ipsa laudantium? Doloremque, delectus!
-                  </p>
-            </div>
-          </div>
      </div>
-    </div>
-  </section>
-  <section>
-    <div class="container mt-3 mb-3">
-      <div class="row ">
-          <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center border-content-blue">
+     <div class="container mt-3 mb-3">  
+        <div class="row ">
+          <div class="col-6 col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center ">
             <img width="150" height="280" src="images/profe johana.jpeg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
             <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
           </div>
-         <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 border-content">
+        <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light text-white">
           <div class="icon-box">
             <div class="icon"><i class="bx bx-fingerprint justify-content-center"></i></div>
-            <h4>Alexandra Ibarra Aguirre</h4>
+            <h3>Alexandra Ibarra Aguirre</h3>
                 <span>Licenciada en Pedagogia Infantil</span>
-                <br><p>Docente </p>
+                <p> Docente </p>
                 <p>Centro Educativo Mi Pequeño Mundo </p>
           </div>
         </div>
-            <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center border-content-blue">
+            <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center ">
               <img width="150" height="280" src="images/trectora.jpeg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
               <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
             </div>
-            <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 border-content">
+            <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light text-white">
               <div class="icon-box">
               <div class="icon"><i class="bx bx-fingerprint"></i></div>
               <h4>Cindy Nayive Diaz Quevedo </h4>
@@ -64,12 +44,12 @@
     </section>
   <section>
   <div class="container mt-3 mb-3">
-    <div class="row ">
-        <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center border-content">
+    <div class="row">
+        <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center ">
           <img width="150" height="280" src="images/profe johana.jpeg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
           <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
         </div>
-       <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 border-content-blue">
+       <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light text-white">
         <div class="icon-box">
           <div class="icon"><i class="bx bx-fingerprint justify-content-center"></i></div>
           <h4>Alexandra Ibarra Aguirre</h4>
@@ -78,11 +58,11 @@
               <p>Centro Educativo Mi Pequeño Mundo </p>
         </div>
       </div>
-          <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center border-content">
+          <div class="col-xl-3 col-lg-3 video-box d-flex justify-content-center align-items-center">
             <img width="150" height="280" src="images/trectora.jpeg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></img>
             <a href="" class="" data-vbtype="video" data-autoplay="true"></a>
           </div>
-          <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 border-content-blue">
+          <div class="col-xl-3 col-lg-3 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-4 bg-color-blue-light text-white">
             <div class="icon-box">
             <div class="icon"><i class="bx bx-fingerprint"></i></div>
             <h4>Cindy Nayive Diaz Quevedo </h4>
--- horarios.php
+++ horarios.php
@@ -27,6 +27,23 @@
            <p class="italic text-center"></p>
         </div>
         <div class="col 12 mt-3">
+          <p class="h4 text-center color-blue mb-3 bg-color-blue-light text-white p-2">HORARIO JARDIN </p>
+        </div>
+      </div>  
+    <div class="row">
+      <div class="container">
+        <div class="col-12 ml-0">
+            <div class="horarios">
+                <a href="images/HorarioJardin.png"
+                    target="_blank">
+                    <img src="images/HorarioJardin.png" alt="Cronograma " class="img-fluid">
+                </a>
+            </div> 
+        </div>
+      </div>
+    </div> 
+    <div class="container mt-3 mb-3">
+        <div class="col 12 mt-3">
           <p class="h4 text-center color-blue mb-3 bg-color-blue-light text-white p-2">HORARIO PREJARDIN </p>
         </div>
       </div>  
@@ -35,6 +52,23 @@
         <div class="col-12 ml-0">
             <div class="horarios">
                 <a href="images/HorarioJardin.png"
+                    target="_blank">
+                    <img src="images/HorarioPrejardin.png" alt="Cronograma " class="img-fluid">
+                </a>
+            </div> 
+        </div>
+      </div>
+    </div> 
+    <div class="container mt-3 mb-3">
+        <div class="col 12 mt-3">
+          <p class="h4 text-center color-blue mb-3 bg-color-blue-light text-white p-2">HORARIO PARVULOS </p>
+        </div>
+      </div>  
+    <div class="row">
+      <div class="container">
+        <div class="col-12 ml-0">
+            <div class="horarios">
+                <a href="images/HorarioParvulos.png"
                     target="_blank">
                     <img src="images/HorarioJardin.png" alt="Cronograma " class="img-fluid">
                 </a>
