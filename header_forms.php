c4852272e17c5765bdc1b88b3f4bf4aba92ac370
darwin0599 <darentimput1999@gmail.com>
2020-11-19 02:38:18 -0500

    menu admin

---
+++ header_forms.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link rel="stylesheet" href="css/forms.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Mi Pequeño Mundo</title>
+</head>
+
+<body>
+<header>
+        <div class="col-2 bg-warning ">
+            <nav class="bg-warning w-100">
+                <h5>Mi pequeño mundo</h5>
+                <hr class="mx-2">
+                <ul class="style_none list-group">
+                    <li class="list-group-item">ADMINISTRADOR</li>
+                    <li class="  d-inline-flex align-items-center list-group-item list-group-item-primary"> <a href="form_list_banner.php"><i class="fas fa-cog"></i> Imagenes y videos</a></li>
+                    <li class="  d-inline-flex align-items-center list-group-item list-group-item-secondary"><a href="form_list_news.php"><i class="fas fa-cog"></i> Noticias y Eventos </a></li>
+                    <li class="  d-inline-flex align-items-center list-group-item list-group-item-success"><a href="form_list_admissions.php"><i class="fas fa-cog"></i> Admisiones y Matriculas </a></li>
+                    <li class=" d-inline-flex align-items-center list-group-item list-group-item-danger"><a href="form_list_honors.php"><i class="fas fa-cog"></i> Cuadro Honores</a></li>
+                    <li class=" d-inline-flex align-items-center list-group-item list-group-item-warning"><a href="form_list_teacher.php"><i class="fas fa-cog"></i> Docentes</a></li>
+                    <li class=" d-inline-flex align-items-center list-group-item list-group-item-info"><a href="form_list_info.php"><i class="fas fa-cog"></i> Información Contacto</a></li>
+                    
+                </ul>
+                <hr class="mx-2">
+                <ul class="style-none">
+
+                    <li class="py-2">
+                        <a href="form_list_banner.php"><i class="fas fa-cog"></i> Imagenes y videos</a>
+                    </li>
+                    <li class="py-">
+                        <a href="form_list_news.php"><i class="fas fa-cog"></i> Noticias y Eventos </a>
+                    </li>
+                    <li class="py-2">
+                        <a href="form_list_admissions.php"><i class="fas fa-cog"></i> Admisiones y Matriculas </a>
+                    </li>
+                    <li class="py-2">
+                        <a href="form_list_honors.php"><i class="fas fa-cog"></i> Cuadro Honores</a>
+                    </li>
+                    <li class="py-2">
+                        <a href="form_list_teacher.php"><i class="fas fa-cog"></i> Docentes</a>
+                    </li>
+                    <li class="py-2">
+                        <a href="form_list_info.php"><i class="fas fa-cog"></i> Información Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        </div>
+    </header>
+</body>
