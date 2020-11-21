9caf96a5a6432a074d3ac0d8f17afea2a927b6c9
jsoler88 <jhonsoler888@gmail.com>
2020-11-21 13:56:27 -0500

    add delete and edit banners

--- form_list_banner.php
+++ form_list_banner.php
@@ -1,7 +1,12 @@
 <?php 
    
     include ("config.php");
-    include ('header_forms.php'); 
+    include ('header_forms.php');
+    if (isset($_GET['id'])) {
+        $sql = "DELETE FROM banners WHERE id = ".$_GET['id'];
+        mysqli_query($con, $sql);
+        unlink($_GET['url_media']);
+    }
     $ret=mysqli_query($con,"SELECT * FROM banners");
 ?>
 
@@ -55,9 +60,11 @@
                                                 <td>'.$row['section'].'</td>
                                                 <td class="text-center">
                                                     <a href="form_edit_banner.php?id='.$row['id'].'" class="btn">
-                                                        <i href="" class="fa fa-edit"></i>
-        
+                                                        <i href="" class="fa fa-edit color-blue"></i>
                                                     </a>
+                                                    <button onclick="test('.$row['id'].', `'.$row['url_media'].'`)" class="btn">
+                                                        <i class="fa fa-trash text-danger"></i>
+                                                    </button>
                                                 </td>
                                             </tr>';
                                             }
@@ -69,17 +76,25 @@
                 </div>
             </div>
         </div>
-
-
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-        </script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-            integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
-        </script>
 </body>
-
+<script type="text/javascript">
+        function test(id, url_media) {
+           Swal
+               .fire({
+                   title: "Venta #123465",
+                   text: "¿Eliminar?",
+                   icon: 'warning',
+                   showCancelButton: true,
+                   confirmButtonText: "Sí, eliminar",
+                   cancelButtonText: "Cancelar",
+               })
+               .then(resultado => {
+                   if (resultado.value) {
+                       // Hicieron click en "Sí"
+                       var newLocation = "form_list_banner.php?id=" + id + "&url_media=" + url_media;
+                       window.location = newLocation;
+                   }
+               });
+        }
+    </script>
 </html>
